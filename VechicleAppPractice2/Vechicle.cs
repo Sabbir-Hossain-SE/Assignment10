c14aa87b154e434cdfa0d5cc03755bf0a9027028
Sabbir <sabbir.hossain133045@gmail.com>
2019-10-14 02:42:10 +0600

    Vechicle

---
+++ VechicleAppPractice2/Vechicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    public class Vechicle
+    {
+        public List<double> speed = new List <double>();
+        public string name, regNo;
+        
+
+        public Vechicle( string name ,string regNo)
+        {
+            this.name = name;
+            this.regNo = regNo;
+        }
+        public double Min()
+        {
+            
+            return speed.Min();
+        }
+        public double Avg()
+        {
+            
+            return speed.Average();
+        }
+        public double Max()
+        {
+
+            return speed.Max();
+        }
+        public void SetSpeed(double speed)
+        {
+            this.speed.Add(speed);
+            
+        }
+
+
+
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        Vechicle vechicle;
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            vechicle = new Vechicle(vehicleNameTextBox.Text, regNoTextBox.Text);
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+
+            vechicle.SetSpeed(Convert.ToDouble(speedTextBox.Text));
+            speedTextBox.Clear();
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = Convert.ToString( vechicle.Min());
+            maxSpeedTextBox.Text = Convert.ToString(vechicle.Max());
+            averageSpeedTextBox.Text = Convert.ToString(vechicle.Avg());
+
+        }
+    }
+}
